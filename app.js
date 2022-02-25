dc4da8c2b348644c7ccde20d40ed232f733f7409
ASIAZURUR <93027041+ASIAZURUR@users.noreply.github.com>
2022-02-25 21:36:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const { google } = require("googleapis");
 const req = require('express/lib/request');
 const dotenv=require('dotenv');
-dotenv.config();
+// dotenv.config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
