c7f667662174b45730f9fd3c2ecdadcef621edc6
ASIAZURUR <93027041+ASIAZURUR@users.noreply.github.com>
2022-02-26 08:27:11 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const dotenv=require('dotenv');
+
 const nodemailer=require('nodemailer');
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,28 +12,28 @@ app.get('/',function(req,res){
     res.send("Welcome to Coding Competition #2 by ASIA ZURUR.K ,  FSD KKEM OCT")
   
 });
-dotenv.config();
+
 const { google } = require("googleapis");
 const req = require('express/lib/request');
 const OAuth2 = google.auth.OAuth2;
 const oauth2Client = new OAuth2(
- process.env.OAUTH_CLIENTID  , // ClientID
-process.env.OAUTH_CLIENT_SECRET , // Client Secret
+'912757938079-rtpi9km0vqlna0o4t3n4omgpaf3mvi6s.apps.googleusercontent.com'  , // ClientID
+'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' , // Client Secret
   "https://developers.google.com/oauthplayground" // Redirect URL
 );
 oauth2Client.setCredentials({
-  refresh_token:process.env.OAUTH_REFRESH_TOKEN ;
+  refresh_token:'1//04BxrBV5EaUM4CgYIARAAGAQSNwF-L9IrXargcdTJdDiuurBf5bX2nUpJ_mTADe2nB2OR3zBZMrfJzrpTZV7KAHWYgEnLBWBRKgU' ;
 });
 const accessToken = oauth2Client.getAccessToken()
     let transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
         type: 'OAuth2',
-        user:process.env.MAIL_USERNAME,
-        pass:process.env.MAIL_PASSWORD,
-        clientId:process.env.OAUTH_CLIENTID,
-        clientSecret:process.env.OAUTH_CLIENT_SECRET,
-        refreshToken:process.env.OAUTH_REFRESH_TOKEN,
+        user:"asiyasuroor@gmail.com",
+        pass:"9526852962",
+        clientId:'912757938079-rtpi9km0vqlna0o4t3n4omgpaf3mvi6s.apps.googleusercontent.com',
+        clientSecret:'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',
+        refreshToken:'1//04BxrBV5EaUM4CgYIARAAGAQSNwF-L9IrXargcdTJdDiuurBf5bX2nUpJ_mTADe2nB2OR3zBZMrfJzrpTZV7KAHWYgEnLBWBRKgU',
         accessToken:accessToken
       }
     });
@@ -44,7 +44,7 @@ app.get('/home',function(req,res){
 app.post('/home',function(req,res){
  console.log(req.body.email);
 var mailOptions = {
-  from:process.env.MAIL_USERNAME,
+  from:'asiyasuroor@gmail.com',
   to: req.body.email,
   subject: 'Sending Email using Node.js',
   text: 'Your email is verified!'
